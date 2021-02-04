c555e91150d7a1323bbf1971aea5ed0e34cdbe34
Md Zakir Hossain Bhuiyan <zakirhossaintuhin@gamil.com>
2021-02-04 23:00:45 +0600

    message

--- js/getInput.js
+++ js/getInput.js
@@ -10,7 +10,6 @@ function getInputValue(value){
 
 function createInput(inputValue){
     let fullInput = document.getElementById('input-display').value;
-    console.log(typeof(fullInput));
     for (let i = 0; i < arguments.length; i++) {
         let partInput = arguments[i];
         fullInput = fullInput + partInput;
